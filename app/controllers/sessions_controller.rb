1d05edda57bf26df5832f50a959ff8717647613f
Mike Schutte <tmikeschutte@gmail.com>
2016-12-20 15:20:43 -0700

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success]   = "Login successful!"
+      redirect_to user_path(@user)
+    else
+    end
+  end
+end
