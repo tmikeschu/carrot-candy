68c316099fce70778688c2767842742c9e1e7997
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 06:17:06 -0700

    Add namespaced rewards resources through admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]  
-  resources :rewards, only: [:new, :index, :create, :show]
+  namespace :admin do
+    resources :rewards, only: [:new, :index, :create, :show]
+  end
 
   get     '/login', to: "sessions#new"
   post    '/login', to: "sessions#create"
