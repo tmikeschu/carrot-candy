79c707c9f0990943941af3f7ce95f97163e9fb5a
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 07:40:59 -0700

    Add update action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]  
   namespace :admin do
-    resources :rewards, only: [:new, :index, :create, :show, :edit]
+    resources :rewards, only: [:new, :index, :create, :show, :edit, :update]
   end
 
   get     '/login', to: "sessions#new"
