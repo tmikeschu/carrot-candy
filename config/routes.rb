c08589e2db0a2512bc82ac6f022b2d994cb13f90
Mike Schutte <tmikeschutte@gmail.com>
2016-12-20 18:13:28 -0700

    Add resources for rewards

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]  
+  resources :rewards, only: [:new, :index, :create, :show]
 
   get     '/login', to: "sessions#new"
   post    '/login', to: "sessions#create"
