b3dfb768b40da4f5b53ca7232bf69761f2813f7b
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 09:33:53 -0700

    Finish feature test for admin edits reward

--- spec/features/rewards/admin_edits_a_reward_spec.rb
+++ spec/features/rewards/admin_edits_a_reward_spec.rb
@@ -24,7 +24,6 @@ RSpec.feature "Admin edits a reward" do
   end
 
   scenario "they can update a rewards information" do
-    # byebug
     within "li##{@rewards.first.name.downcase.split(" ").join}" do
       expect(page).to_not have_content "iPod"
       click_on "Edit Reward"
@@ -35,10 +34,49 @@ RSpec.feature "Admin edits a reward" do
     click_on "Update Reward"
     expect(page).to have_content "Update successful!"
     expect(page).to_not have_content @rewards.first.name
-    within "li#iPod" do
+    within "li#ipod" do
       expect(page).to have_content "iPod"
       expect(page).to have_content 5
       expect(page).to have_content 20
+    end
+  end
+
+  context "when they submit empty information" do
+    scenario "they see an error" do
+      within "li##{@rewards.first.name.downcase.split(" ").join}" do
+        expect(page).to_not have_content "iPod"
+        click_on "Edit Reward"
+      end
+      fill_in "reward_name", with: ""  
+      fill_in "reward_quantity", with: nil 
+      fill_in "reward_point_value", with: nil 
+      click_on "Update Reward"
+      expect(page).to have_content "Name can't be blank"
+      expect(page).to have_content "Quantity can't be blank"
+      expect(page).to have_content "Point value can't be blank"
+    end
+  end
+
+  context "when a user is logged in" do
+    before do
+      click_on "Log out"
+      within "form" do
+        click_on "Log in"
+      end
+      fill_in "email", with: @user.email
+      fill_in "password", with: @user.password
+      within "form" do
+        click_on "Log in"
+      end
+    end
+
+    scenario "they cannot see an edit link" do
+      expect(page).to_not have_content "Edit Reward"
+    end
+
+    scenario "they cannot override with the url" do
+      visit edit_admin_reward_path(@rewards.first)
+      expect(page).to have_content "The page you were looking for doesn't exist."
     end
   end
 end
