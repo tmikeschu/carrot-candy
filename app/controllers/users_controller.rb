7730a556b5ce7ecea9150f4e98446f32cc3f5ec1
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 17:15:15 -0700

    Add buy and add_reward actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def buy
+    @reward = Reward.find(params[:id])
+    if @reward.point_value > current_user.points
+      flash[:error] = "Whoops! You do not have enough points for that reward."
+      redirect_to rewards_path
+    else
+    end
+  end
+
+  def add_reward
+    @reward = Reward.find(params[:id])
+    current_user.rewards << @reward
+    @reward.quantity - 1
+    @reward.save
+    current_user.points -= @reward.point_value
+    current_user.redeemed_points += @reward.point_value
+    current_user.save 
+    redirect_to user_path(current_user)
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password)
