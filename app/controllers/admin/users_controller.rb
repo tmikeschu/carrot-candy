e8ee9cc83f097aadf6d8d2b6a6deb8c7abba0b1e
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 20:56:05 -0700

    Add edit, show, and update, with before methods

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new 
   end
@@ -19,8 +20,29 @@ class Admin::UsersController < Admin::BaseController
     @users = User.where(role: "default")
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    @user.points += point_params[:points].to_i
+    @user.save
+    flash[:success] = "#{point_params[:points]} points added to #{@user.name}"
+    redirect_to admin_users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+    def point_params
+      params.require(:user).permit(:points)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
