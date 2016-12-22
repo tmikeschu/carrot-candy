5389af965a226f2cbfeb30445dd534898c925a0f
Mike Schutte <tmikeschutte@gmail.com>
2016-12-21 20:55:49 -0700

    Add name method and on create to password valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {in: 10..50}
+  validates :password, presence: true, length: {in: 10..50}, on: :create
 
   enum role: %w(default admin)
 
   has_many :users_rewards
   has_many :rewards, through: :users_rewards
+
+  def name
+    self.first_name + " " + self.last_name
+  end
 end
