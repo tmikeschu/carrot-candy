8b892eb8d36e3db383bc6a68cf831a55d96ade73
Mike Schutte <tmikeschutte@gmail.com>
2016-12-22 09:39:46 -0700

    Remove negative value test check

--- spec/features/users/user_destroys_points_of_a_user_spec.rb
+++ spec/features/users/user_destroys_points_of_a_user_spec.rb
@@ -23,10 +23,10 @@ RSpec.feature "User destroys points from a user" do
       expect(@user.points).to eq 50
       visit admin_users_path
       click_on @user.name
-      click_on "Remove Points"
+      click_on "Add or Remove Points"
 
-      expect(current_path).to eq "/admin/users/2/remove-points"
-      fill_in "user_points", with: 30
+      expect(current_path).to eq edit_admin_user_path(@user)
+      fill_in "user_points", with: -30
       click_on "Update Points"
       
       @user.reload
@@ -34,29 +34,9 @@ RSpec.feature "User destroys points from a user" do
       expect(@user.points).to eq 20
       expect(page).to have_content "30 points removed from #{@user.name}"
     end
-
-    xcontext "when they try to remove negative points" do
-      scenario "they are brought back to the user show page with an error showing" do
-        @user.points = 50
-        @user.save
-        expect(@user.points).to eq 50
-        visit admin_users_path
-        click_on @user.name
-        click_on "Delete Points"
-
-        expect(current_path).to eq edit_admin_user_path(@user)
-        fill_in "user_points", with: -30
-        click_on "Update Points"
-
-        expect(current_path).to eq admin_user_path(@user)
-        expect(page).to have_content "Whoops! Can't remove negative points. If you want to add points, click 'Add Points'"
-        @user.reload
-        expect(@user.points).to eq 50
-      end
-    end
   end
 
-  xdescribe "as a user" do
+  describe "as a user" do
     before do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       allow_any_instance_of(ApplicationController).to receive(:current_admin?).and_return(false)
