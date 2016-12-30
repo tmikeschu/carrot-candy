42176268f99d05481c71a16e8be12eefd8cb339f
Mike Schutte <tmikeschutte@gmail.com>
2016-12-30 15:35:54 -0700

    Add factory seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'factory_girl_rails'
+
+FactoryGirl.create_list(:user, 10)
+
+FactoryGirl.create_list(:reward, 10)
